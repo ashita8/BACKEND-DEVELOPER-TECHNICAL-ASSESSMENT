6da333727f473b7eb1fe1222850dd8737aa6e7b6
Ashita Aswal <ashita26102000@gmail.com>
2026-04-02 11:44:30 +0530

    load customer data from JSON file and updatedd the dockerfile and requirements file location.

--- mock-server/app.py
+++ mock-server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,request
+import json
 
 app = Flask(__name__)
 
@@ -6,5 +7,24 @@ app = Flask(__name__)
 def health():
     return {"status": "ok"}
 
+with open("data/customers.json") as f:
+    customers = json.load(f)
+
+@app.route("/api/customers")
+def get_customers():
+    page = int(request.args.get("page", 1))
+    limit = int(request.args.get("limit", 10))
+
+    start = (page - 1) * limit
+    end = start + limit
+
+    return {
+        "data": customers[start:end],
+        "total": len(customers),
+        "page": page,
+        "limit": limit
+    }
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
